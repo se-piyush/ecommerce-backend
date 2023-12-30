af204a9393eda9776ed59b316b05adff4825e19d
Piyush Sharma <sepiyusharma@gmail.com>
2023-12-31 04:20:46 +0530

    added migrations user-service and updated dockerfile

--- user-service/src/utils/token.ts
+++ user-service/src/utils/token.ts
@@ -1,11 +1,18 @@
 import jwt from "jsonwebtoken";
-const secret = process.env.SECRET || "this is a secret key";
-export const generateToken = (key: string, value: string): string => {
-  return jwt.sign({ [key]: value }, secret, {
-    expiresIn: "1h",
-  });
-};
 
-export const verifyToken = (token: string): any => {
-  return jwt.verify(token, secret);
-};
+class Token {
+  secret = process.env.SECRET || "this is a secret key";
+  generateToken = (key: string, value: string): string => {
+    return jwt.sign({ [key]: value }, this.secret, {
+      expiresIn: "1h",
+    });
+  };
+  verifyToken = (token: string): any => {
+    return jwt.verify(token, this.secret);
+  };
+  changeSecret = () => {
+    this.secret = (Math.random() * 1e32).toString(36);
+  };
+}
+
+export default new Token();
