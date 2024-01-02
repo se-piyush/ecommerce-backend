2febb0ef7f696701cd4d8de50291ee6dcee5d2f7
Piyush Sharma <sepiyusharma@gmail.com>
2024-01-03 01:03:01 +0530

    updated db.ts code to prevent jest error

--- user-service/src/db.ts
+++ user-service/src/db.ts
@@ -1,6 +1,9 @@
 import { Sequelize } from "sequelize";
-import * as dbConfig from "./config/database.config";
-const dbConfigurationObject = (<any>dbConfig).development;
+
+// added this syntax to bypass jest could not resolve database.config.ts' is not a module error
+const { development } = require("./config/database.config");
+
+const dbConfigurationObject = development;
 const sequelize = new Sequelize(
   dbConfigurationObject.database,
   dbConfigurationObject.username,
