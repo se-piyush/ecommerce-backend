0e9afec441b21cf4c070306332cb53c321f19862
Piyush Sharma <sepiyusharma@gmail.com>
2024-01-08 19:09:49 +0530

    added code for central error handling

--- order-service/src/app.ts
+++ order-service/src/app.ts
@@ -1,10 +1,11 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import orderRoutes from "./route/order.router";
 import publicOrderStatusRouter from "./route/orderStatus.router";
 import { authenticateUserMiddleware } from "./middleware/authenticateUser.middleware";
 import { health, ready } from "./controller/order.controller";
 import { updateOrderStatus } from "./controller/orderStatus.controller";
+import BaseError from "./error";
 
 const app = express();
 const PORT = parseInt(process.env.PORT || "5000");
@@ -20,6 +21,11 @@ app.put(`/internal/${rootApipath}/:OrderId/status`, updateOrderStatus);
 app.use(`/${rootApipath}`, authenticateUserMiddleware);
 app.use(`/${rootApipath}`, orderRoutes);
 app.use(`/${rootApipath}`, publicOrderStatusRouter);
+
+app.use((err: BaseError, _req: Request, res: Response, _next: NextFunction) => {
+  console.log(err.message);
+  return res.status(err.httpCode).send(err.message);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- order-service/src/enum.ts
+++ order-service/src/enum.ts
@@ -5,3 +5,13 @@ export enum OrderStatusEnum {
   orderConfirmed = "order confirmed",
   orderOutOfStock = "order out of stock",
 }
+
+export enum HttpStatusCodes {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER = 500,
+  FORBIDDEN = 403,
+  UN_AUTHENTICATED = 401,
+}
